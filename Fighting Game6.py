ca4a2590298ad7663749af28f370dbf345cef606
AbidaAshraf <125407187+AbidaAshraf@users.noreply.github.com>
2023-02-15 13:26:18 +0000

    Add files via upload

--- Fighting Game6.py
+++ Fighting Game6.py
@@ -23,12 +23,34 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.speed = 5
-        self.move = (x, y)
         self.health = 100
+        self.max_health = 100
+        self.health_bar_width = 100
+        self.health_bar_height = 10
 
-player1 = Player(100,230,"Warrior Idle.png")
-player2 = Player(300,200,"Wizard Idle.png")
+     def move(self, direction):
+        speed = 5
+        if direction == "left":
+            self.rect.x -= speed
+            self.facing_right = False
+        elif direction == "right":
+            self.rect.x += speed
+            self.facing_right = True
+
+    def attack(self, other_player):
+        damage = 10
+        if self.rect.colliderect(other_player.rect):
+            other_player.health -= damage
+
+    def draw_health_bar(self):
+        # Draw the player's health bar at the top of the screen
+        health_bar_x = self.rect.x
+        health_bar_y = self.rect.y - 20
+        pygame.draw.rect(screen, (255, 0, 0), (health_bar_x, health_bar_y, self.health_bar_width, self.health_bar_height))
+        pygame.draw.rect(screen, (0, 255, 0), (health_bar_x, health_bar_y, self.health_bar_width * (self.health / self.max_health), self.health_bar_height))
+
+player1 = Player(200,400,"Warrior Idle.png")
+player2 = Player(600,365,"Wizard Idle.png")
 
 players = pygame.sprite.Group()
 players.add(player1)
@@ -38,11 +60,26 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-    
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player1.move("left")
+            elif event.key == pygame.K_RIGHT:
+                player1.move("right")
+            elif event.key == pygame.K_SPACE:
+                player1.attack(player2)
+
+    # Draw the background and player health bars
+    screen.blit(background_image, background_position)
+    for player in players:
+        player.draw_health_bar()
+
+    # Update and draw the players
     players.update()
     players.draw(screen)
-    pygame.display.update()
+
+    # Update the display and wait for the next frame
+    pygame.display.flip()
     clock.tick(60)
-    
 
 pygame.quit()
+
